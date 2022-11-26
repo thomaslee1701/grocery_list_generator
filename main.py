025f87ca12315faf355926074ca499e5fc7c84ef
thomaslee1701 <tcl1701@gmail.com>
2022-11-25 19:58:43 -0800

    Changed to not use pandas

--- main.py
+++ main.py
@@ -1,53 +1,68 @@
-import pandas as pd
+import csv
+from dataclasses import dataclass
+from collections import defaultdict
+"""
+Structure of the grocery list csv
 
-grocery_df = pd.read_excel('grocery_list.xlsx')
+product_name | quantity | location | notes
 
 """
-Structure of the grocery list xlsx
 
-Product Name | Quantity | Location | Notes
+@dataclass
+class ShoppingItem:
+    product_name: str
+    quantity: str
+    location: str
+    notes: str
 
-"""
+class GroceryList:
+    columns = ('product_name', 'quantity', 'location', 'notes')
 
-def pad_string(s, pad_length):
-    return s + ' ' * (pad_length - len(s))
+    def __init__(self, grocerycsv) -> None:
+        self.shoppingItems = []
+        self.shoppingListDict = defaultdict(list)
+        self.parseGroceryXlsx(grocerycsv)
+        self.initializeShoppingListDict(self.shoppingItems)
+        
+    def parseGroceryXlsx(self, grocerycsv):
+        with open(grocerycsv, newline='') as f:
+            reader = csv.reader(f)
+            reader.__next__()
+            for row in reader: # Don't want to read in column names row
+                attrs = dict((col, val) for col, val in zip(self.columns, row))
+                if attrs['quantity'] != '0' and attrs['quantity'] != '':
+                    self.shoppingItems.append(ShoppingItem(**attrs))
+    
+    def initializeShoppingListDict(self, shoppingItems):
+        for item in shoppingItems:
+            self.shoppingListDict[item.location].append(item)
 
-# Remove all entries with quantity 0 or N/A
-grocery_df = grocery_df[(~pd.isna(grocery_df['Quantity'])) & (~(grocery_df['Quantity']==0))]
+    def formatItemsText(self, productNamesList, quantitiesList, notesList):
+        """Returns three lists with properly padded words
 
-location_names = grocery_df['Location'].dropna().unique()
+        """
+        maxProductNameLength = len(max(productNamesList, key=len))
+        maxQuantitiesLength = len(max(quantitiesList, key=len))
+        maxNoteslist = len(max(notesList, key=len))
+        newProduct, newQuantities, newNotes = [], [], []
+        for prod, quant, note in zip(productNamesList, quantitiesList, notesList):
+            newProduct.append(prod + ' ' * (maxProductNameLength - len(prod)) + '\t')
+            newQuantities.append(quant + ' ' * (maxQuantitiesLength - len(quant)) + '\t\t')
+            newNotes.append(note + ' ' * (maxNoteslist - len(note)))
+        return newProduct, newQuantities, newNotes
 
-# Write to the txt file
-f = open('list.txt', 'w')
-f.close()
+    def exportShoppingListTxt(self):
+        with open('GROCERYLIST.txt', 'w') as f:
+            for loc, itemsList in self.shoppingListDict.items():
+                f.write('-' * (len(loc) * 3) + '\n')
+                f.write('-' * (len(loc)-1) + ' ' + loc.upper() + ' ' + '-' * (len(loc)-1) + '\n')
+                f.write('-' * (len(loc) * 3) + '\n')
+                productNames, quantities, notes = [item.product_name for item in itemsList], [item.quantity for item in itemsList], [item.notes for item in itemsList]
+                for prod, quant, note in zip(*self.formatItemsText(productNames, quantities, notes)):
+                    f.write(f'{prod}{quant}{note}\n')
+                f.write('\n')
 
-with open('list.txt', 'a') as f:
-    for location in location_names:
-        grocery_df_temp = grocery_df[grocery_df['Location'] == location]
-        product_names = grocery_df_temp['Product Name']
-        grocery_df_temp = grocery_df_temp.set_index('Product Name')
-        biggest_product_name = max([len(name) for name in product_names])
-        f.write(f'--------------------\n{location.upper()}\n')
-        for name in product_names:
-            sr = grocery_df_temp.loc[name]
-            quantity = sr['Quantity']
-            if type(quantity) != str and quantity % 1 == 0:
-                quantity = int(quantity)
-            notes = sr['Notes']
-            f.write(f'{pad_string(name, biggest_product_name + 4)} {pad_string(str(quantity), 6)} {notes} \n')
-        f.write('--------------------\n\n')    
-    # Now all the ones with no location specified
-    grocery_df_temp = grocery_df[~(grocery_df['Location'].isin(location_names))]    
-    product_names = grocery_df_temp['Product Name']
-    grocery_df_temp = grocery_df_temp.set_index('Product Name')
-    biggest_product_name = max([len(name) for name in product_names])
-    for name in product_names:
-        sr = grocery_df_temp.loc[name]
-        quantity = sr['Quantity']
-        if type(quantity) != str and quantity % 1 == 0:
-            quantity = int(quantity)
-        notes = sr['Notes']
-        f.write(f'{pad_string(name, biggest_product_name + 4)} {pad_string(str(quantity), 6)} {notes} \n')
-
+GL = GroceryList('grocery_list.csv')
+GL.exportShoppingListTxt()
 print('done!')
 
